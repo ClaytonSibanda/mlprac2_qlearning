9fc4f8daf5ea8486d54ad5f424807a5d2cd6a942
Muhummad Patel <muhummad.patel@gmail.com>
2015-10-15 16:14:32 +0200

    Allocate space for the QTables for each minesweeper.

--- project/CQLearningController.cpp
+++ project/CQLearningController.cpp
@@ -14,12 +14,26 @@ for a detailed discussion on Q Learning
 */
 #include "CQLearningController.h"
 
+#include <vector>
+
 
 CQLearningController::CQLearningController(HWND hwndMain):
 	CDiscController(hwndMain),
 	_grid_size_x(CParams::WindowWidth / CParams::iGridCellDim + 1),
 	_grid_size_y(CParams::WindowHeight / CParams::iGridCellDim + 1)
 {
+	printf("gx: %d   gy: %d\n", _grid_size_x, _grid_size_y);
+
+	InitializeLearningAlgorithm();
+
+	//for (uint y = 0; y < _grid_size_y; y++){
+	//	for (uint x = 0; x < _grid_size_x; x++){
+	//		for (uint action = 0; action < 4; action++){
+	//			printf("%.0f ", _Q_sx_sy_a[x][y][action]);
+	//		}
+	//	}
+	//	printf("\n");
+	//}
 }
 /**
  The update method should allocate a Q table for each sweeper (this can
@@ -30,6 +44,26 @@ CQLearningController::CQLearningController(HWND hwndMain):
 void CQLearningController::InitializeLearningAlgorithm(void)
 {
 	//TODO
+
+	//initialize the Q-tables with all 0's
+	for (uint sweeper = 0; sweeper < m_NumSweepers; sweeper++){
+		std::vector<std::vector<std::vector<double > > > q_table;
+			for (uint x = 0; x < _grid_size_x; x++){
+				std::vector<std::vector<double> > state_y;
+				for (uint y = 0; y < _grid_size_y; y++){
+					std::vector<double> actions;
+					for (uint action = 0; action < 4; action++){
+						actions.push_back(0.0);
+					}
+
+					state_y.push_back(actions);
+				}
+
+				q_table.push_back(state_y);
+			}
+
+			_Q_sx_sy_a.push_back(q_table);
+	}
 }
 /**
  The immediate reward function. This computes a reward upon achieving the goal state of
--- project/CQLearningController.h
+++ project/CQLearningController.h
@@ -4,6 +4,8 @@
 #include "CDiscCollisionObject.h"
 #include <cmath>
 
+#include <vector>
+
 typedef unsigned int uint;
 class CQLearningController :
 	public CDiscController
@@ -11,6 +13,9 @@ class CQLearningController :
 private:
 	uint _grid_size_x;
 	uint _grid_size_y;
+
+	std::vector<std::vector<std::vector<std::vector<double> > > > _Q_sx_sy_a; //up-right-down-left == 0-1-2-3
+
 public:
 	CQLearningController(HWND hwndMain);
 	virtual void InitializeLearningAlgorithm(void);
