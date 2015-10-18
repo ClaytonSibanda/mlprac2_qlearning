b5c25504275e6402d42b73e95c030bdd43074c97
muhummadPatel <muhummad.patel@gmail.com>
2015-10-18 12:02:01 +0200

    Clean up and document. All working well.

--- project/CQLearningController.cpp
+++ project/CQLearningController.cpp
@@ -22,30 +22,14 @@ CQLearningController::CQLearningController(HWND hwndMain):
 	_grid_size_x(CParams::WindowWidth / CParams::iGridCellDim + 1),
 	_grid_size_y(CParams::WindowHeight / CParams::iGridCellDim + 1)
 {
-	printf("gx: %d   gy: %d\n", _grid_size_x, _grid_size_y);
-
-	//InitializeLearningAlgorithm();
-
-	//for (uint y = 0; y < _grid_size_y; y++){
-	//	for (uint x = 0; x < _grid_size_x; x++){
-	//		for (uint action = 0; action < 4; action++){
-	//			printf("%.0f ", _Q_sx_sy_a[x][y][action]);
-	//		}
-	//	}
-	//	printf("\n");
-	//}
 }
-/**
- The update method should allocate a Q table for each sweeper (this can
- be allocated in one shot - use an offset to store the tables one after the other)
 
- You can also use a boost multiarray if you wish
+/**
+ Allocates space for the Q Table and the dead table and initializes them appropriately.
 */
 void CQLearningController::InitializeLearningAlgorithm(void)
 {
-	//TODO
-
-	//initialize the Q-table with all 0's
+	//initialize the Q-table with all 0's. Initially, every action in every state has neutral reard.
 	for (uint x = 0; x < _grid_size_x; x++){
 		std::vector<std::vector<double> > state_y;
 		for (uint y = 0; y < _grid_size_y; y++){
@@ -55,54 +39,65 @@ void CQLearningController::InitializeLearningAlgorithm(void)
 		_Q_sx_sy_a.push_back(state_y);
 	}
 
+	//Allocate & initialize the dead table
 	resetDeadCheck();
 }
 
+/**
+ Initializes the deadCheck table. The dead Check table is used to ensure that when the agents die,
+ They update the Qtable to reflect the negative reward resulting from that action in that particular state.
+*/
 void CQLearningController::resetDeadCheck(){
 	deadCheck.clear();
 	for (int i = 0; i < m_NumSweepers; i++){
 		deadCheck.push_back(false);
 	}
 }
+
 /**
- The immediate reward function. This computes a reward upon achieving the goal state of
- collecting all the mines on the field. It may also penalize movement to encourage exploring all directions and 
- of course for hitting supermines/rocks!
+ This is the reward function. It dispenses a reward to the given sweeper that has just transitioned into the state
+ represented by x and y. If the new state is favourable (picked up a mine), a hihg positive rewasrd is given. If the
+ state is negative, drove into a rock or supermine, then a negative reward is given to "punish" this bad action.
 */
 double CQLearningController::R(uint x, uint y, uint sweeper_no){
-	//TODO: roll your own here!
-	//return 0;
-
 	double reward = 0.0f; //neutral reward
 
-	//see what the minesweeper hit
+	//see what the minesweeper hit and adjust the reward accordingly
 	int hit_index = ((m_vecSweepers[sweeper_no])->CheckForObject(m_vecObjects, CParams::dMineScale));
 	if (hit_index >= 0)
 	{
 		switch (m_vecObjects[hit_index]->getType()){
 			case CDiscCollisionObject::Mine:{
 				if (!m_vecObjects[hit_index]->isDead()){
-					printf("NOTDEAD");
+					//If we hit a mine, return a nice positve reward
 					reward = 100.0;
 				}
 				break;
 			}
 			case CDiscCollisionObject::Rock:{
+				//we hit a rock and died, so return a negative reward
 				reward = -100.0;
 				break;
 			}
 			case CDiscCollisionObject::SuperMine:{
+				//we hit a supermine and died, returna negative reward
 				reward = -100.0;
 				break;
 			}
 		}
 	}
-	//if (reward != 0)
-	//	printf("%d reward = %f\n", sweeper_no, reward);
+
 	return reward;
 }
 
+/**
+ Takes in a list of actions and returns the action that has historically, 
+ yielded the highest reward. Where there are mutiple maximum valued actions,
+ one of these actions is randomly chosen. This allows the agents to explore 
+ the environment before beginning to exploit.
+*/
 int getMaxAct(std::vector<double> actions){
+	//Find the maximum.
 	int max_act = 0;
 	double max_val = actions[max_act];
 	for (uint a = 1; a < 4; a++){
@@ -112,19 +107,23 @@ int getMaxAct(std::vector<double> actions){
 		}
 	}
 
+	//find all the maximum valued actions
 	std::vector<int> maxs;
 	for (uint i = 0; i < 4; i++){
 		if (actions[i] == max_val){
-		//if (actions[i] == max_val){
 			maxs.push_back(i);
 		}
 	}
 
+	//return one of the maximum valued actions
 	int ri = RandInt(0, maxs.size() - 1);
 	return maxs[ri];
-	//return max_act;
 }
 
+/**
+ Takes in a list of actions and returns the maximum value given by any of the actions.
+ i.e. the expected reward that will be obtained if the agen behaves optimally.
+*/
 int getMaxActVal(std::vector<double> actions){
 	double max_val = actions[0];
 	for (uint a = 1; a < 4; a++){
@@ -133,106 +132,70 @@ int getMaxActVal(std::vector<double> actions){
 		}
 	}
 
-	std::vector<int> maxs;
-	for (uint i = 0; i < 4; i++){
-		if (actions[i] == max_val){
-			maxs.push_back(actions[i]);
-		}
-	}
-
-	int ri = RandInt(0, maxs.size() - 1);
-	return maxs[ri];
-	//return max_val;
+	return max_val;
 }
 
 /**
 The update method. Main loop body of our Q Learning implementation
 See: Watkins, Christopher JCH, and Peter Dayan. "Q-learning." Machine learning 8. 3-4 (1992): 279-292
 */
-
 bool CQLearningController::Update(void)
 {
-	//m_vecSweepers is the array of minesweepers
-	//everything you need will be m_[something] ;)
 	uint cDead = std::count_if(m_vecSweepers.begin(), m_vecSweepers.end(), [](CDiscMinesweeper * s)->bool{return s->isDead();});
 	if (cDead == CParams::iNumSweepers){
 		printf("All dead ... skipping to next iteration\n");
 		m_iTicks = CParams::iNumTicks;
 
+		//reset our deadCheck table
 		resetDeadCheck();
 	}
 
 	for (uint sw = 0; sw < CParams::iNumSweepers; ++sw){
 		if (m_vecSweepers[sw]->isDead()) continue;
-		/**
-		Q-learning algorithm according to:
-		Watkins, Christopher JCH, and Peter Dayan. "Q-learning." Machine learning 8. 3-4 (1992): 279-292
-		*/
+
 		//1:::Observe the current state:
-		//TODO
 		SVector2D<int> pos = m_vecSweepers[sw]->Position();
-		pos.x /= (CParams::iGridCellDim);
-		pos.y /= (CParams::iGridCellDim);
+		pos /= 10;
 
 		//2:::Select action with highest historic return:
-		//TODO
 		int max_act = getMaxAct(_Q_sx_sy_a[pos.x][pos.y]);
 		m_vecSweepers[sw]->setRotation((ROTATION_DIRECTION)max_act);
-
-		//now call the parents update, so all the sweepers fulfill their chosen action
 	}
-	
+
+	//now call the parents update, so all the sweepers fulfill their chosen action
 	CDiscController::Update(); //call the parent's class update. Do not delete this.
 	
 	for (uint sw = 0; sw < CParams::iNumSweepers; ++sw){
-		//TODO: just use bool cached from the first loop
 		if (m_vecSweepers[sw]->isDead() && deadCheck[sw]){
+			//if we died and already updated the Q table, just skip/continue
 			continue; 
 		}
 		else if(m_vecSweepers[sw]->isDead()){
+			//if we just died, then run  through the loop to update the Qtable with the
+			//negative reward from dying. To ensure that this only happens once after we die,
+			//set deadcheck to true.
 			deadCheck[sw] = true;
 		}
 
-		//TODO:compute your indexes.. it may also be necessary to keep track of the previous state
 		//3:::Observe new state:
-		//TODO
 		SVector2D<int> prev_pos = m_vecSweepers[sw]->PrevPosition();
-		prev_pos.x /= (CParams::iGridCellDim);
-		prev_pos.y /= (CParams::iGridCellDim);
+		prev_pos /= 10;
 
 		SVector2D<int> pos = m_vecSweepers[sw]->Position();
-		pos.x /= (CParams::iGridCellDim);
-		pos.y /= (CParams::iGridCellDim);
+		pos /= 10;
 
 		int action = (int)m_vecSweepers[sw]->getRotation();
 
 		//4:::Update _Q_s_a accordingly:
-		//TODO
-		//_Q_sx_sy_a[sw][prev_pos.x][prev_pos.y][action] = R(pos.x, pos.y, sw)
-		//	+ (gamma_discount_rt * (
-		//	((1 - lambda_learn_rt) * getMaxActVal(_Q_sx_sy_a[sw][pos.x][pos.y]))
-		//	+ (lambda_learn_rt * _Q_sx_sy_a[sw][pos.x][pos.y][action])
-		//	)
-		//	);
-
-		//_Q_sx_sy_a[prev_pos.x][prev_pos.y][action] += (lambda_learn_rt * (R(pos.x, pos.y, sw) + (gamma_discount_rt * getMaxActVal(_Q_sx_sy_a[pos.x][pos.y])) - getMaxActVal(_Q_sx_sy_a[prev_pos.x][prev_pos.y])));
-
 		_Q_sx_sy_a[prev_pos.x][prev_pos.y][action] += (lambda_learn_rt * (R(pos.x, pos.y, sw) + (gamma_discount_rt * getMaxActVal(_Q_sx_sy_a[pos.x][pos.y])) - _Q_sx_sy_a[prev_pos.x][prev_pos.y][action]));
-		//printf("QVAL: %f\n", _Q_sx_sy_a[prev_pos.x][prev_pos.y][action]);
 	}
 
 	if (m_iTicks == CParams::iNumTicks){
+		//if the iteration is over, all the sweepers will be respawned in the next iteration, so reset
+		//the deadcheck table so it will remain valid.
 		resetDeadCheck();
 	}
 
-	/*printf("====================================================================================\n");
-	for (uint y = 0; y < _grid_size_y; y++){
-		for (uint x = 0; x < _grid_size_x; x++){
-			printf("%.2f ", getMaxActVal(_Q_sx_sy_a[x][y]));
-		}
-		printf("\n");
-	}
-	printf("====================================================================================\n");*/
 	return true;
 }
 
--- project/CQLearningController.h
+++ project/CQLearningController.h
@@ -14,11 +14,15 @@ private:
 	uint _grid_size_x;
 	uint _grid_size_y;
 
+	//Q-table
+	//NOTE: one shared Q table to allow for faster learning and convergence(shared knowledge).
 	std::vector<std::vector<std::vector<double> > > _Q_sx_sy_a; //right-up-left-down == 0-1-2-3
 
+	//Q-learning paramaters (chosen experimentally)
 	double gamma_discount_rt = 0.8;
 	double lambda_learn_rt = 0.1;
 
+	//To allow for updating the Q-table once after the sweeper has died
 	std::vector<bool> deadCheck;
 	void resetDeadCheck();
 
